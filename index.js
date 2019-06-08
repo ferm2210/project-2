bcae5846baefb0b8645f2f0d0383b163576ebc07
FERM <ferm2210@gmail.com>
2019-06-08 13:01:17 -0500

    añadiendo script1.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const violet = document.getElementById("violet");
 const orange = document.getElementById("orange");
 const green = document.getElementById("green");
 const btnStart = document.getElementById("btnStart");
+
+const hover = () => {
+  skyblue.style.background = "blue";
+};
+
+skyblue.addEventListener("click", hover);
