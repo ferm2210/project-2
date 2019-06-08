6a771975d9d2854082c62e8fbf50f90040ef9532
FERM <ferm2210@gmail.com>
2019-06-08 11:40:08 -0500

    Proyect beginning: index.html, style.css, index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const skyblue = document.getElementById("skyblue");
+const violet = document.getElementById("violet");
+const orange = document.getElementById("orange");
+const green = document.getElementById("green");
+const btnStart = document.getElementById("btnStart");
