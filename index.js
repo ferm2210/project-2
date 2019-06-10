20e0c6111defdc7227d1bd5a8c158bd22e329756
FERM <ferm2210@gmail.com>
2019-06-10 12:48:10 -0500

    iluminado y mapeo de colores y numeros

--- index.js
+++ index.js
@@ -4,8 +4,46 @@ const orange = document.getElementById("orange");
 const green = document.getElementById("green");
 const btnStart = document.getElementById("btnStart");
 
-const hover = () => {
-  skyblue.style.background = "blue";
+const hide = () => {
+  btnStart.classList.add("hide");
 };
 
-skyblue.addEventListener("click", hover);
+btnStart.addEventListener("click", hide);
+
+const randomNumbers = amount =>
+  new Array(amount).fill(0).map(n => Math.floor(Math.random() * 4));
+
+const numberToColor = number => {
+  switch (number) {
+    case 0:
+      return "skyblue";
+    case 1:
+      return "violet";
+    case 2:
+      return "orange";
+    case 3:
+      return "green";
+  }
+};
+
+const colorToNumber = color => {
+  switch (color) {
+    case "skyblue":
+      return 0;
+    case "violet":
+      return 1;
+    case "orange":
+      return 2;
+    case "green":
+      return 3;
+  }
+};
+
+const enlighten = ev => {
+  ev.srcElement.classList.add("light");
+  setTimeout(() => {
+    ev.srcElement.classList.remove("light");
+  }, 200);
+};
+
+document.body.addEventListener("click", enlighten);
